7a28bb4ebe6137131552617d1633fbfdf28337e2
Aljonator <121176500+AljonGabriel@users.noreply.github.com>
2023-11-18 23:31:45 +0800

    Doing Changes

--- BGAnimation.js
+++ BGAnimation.js
@@ -1,90 +1,90 @@
 // Array of objects containing PNG images with their respective locations
 const pngImages = [
   {
-    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png",
-    location: {x: 600, y: 50},
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png',
+    location: { x: 600, y: 50 },
     delay: 1,
   },
   {
-    src: "https://cdn-icons-png.flaticon.com/512/1310/1310190.png",
-    location: {x: 1000, y: 400},
+    src: 'https://cdn-icons-png.flaticon.com/512/1310/1310190.png',
+    location: { x: 1000, y: 400 },
     delay: 2,
   },
   {
-    src: "https://static.thenounproject.com/png/424728-200.png",
-    location: {x: 700, y: 190},
+    src: 'https://static.thenounproject.com/png/424728-200.png',
+    location: { x: 700, y: 190 },
 
     delay: 3,
   },
   {
-    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png",
-    location: {x: 100, y: 400},
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png',
+    location: { x: 100, y: 400 },
     delay: 4,
   },
   {
-    src: "https://cdn-icons-png.flaticon.com/512/1310/1310190.png",
-    location: {x: 400, y: 600},
+    src: 'https://cdn-icons-png.flaticon.com/512/1310/1310190.png',
+    location: { x: 400, y: 600 },
     delay: 5,
   },
   {
-    src: "https://static.thenounproject.com/png/424728-200.png",
-    location: {x: 900, y: 300},
+    src: 'https://static.thenounproject.com/png/424728-200.png',
+    location: { x: 900, y: 300 },
     delay: 6,
   },
 
   //
 
   {
-    src: "https://cdn-icons-png.flaticon.com/512/1310/1310190.png",
-    location: {x: 300, y: 100},
+    src: 'https://cdn-icons-png.flaticon.com/512/1310/1310190.png',
+    location: { x: 300, y: 100 },
     delay: 6,
   },
   {
-    src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png",
-    location: {x: 850, y: 500},
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png',
+    location: { x: 850, y: 500 },
     delay: 5,
   },
   {
-    src: " https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png",
-    location: {x: 900, y: 100},
+    src: ' https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/768px-Question_mark_%28black%29.svg.png',
+    location: { x: 900, y: 100 },
 
     delay: 4,
   },
   {
-    src: "https://static.thenounproject.com/png/424728-200.png",
-    location: {x: 200, y: 300},
+    src: 'https://static.thenounproject.com/png/424728-200.png',
+    location: { x: 200, y: 300 },
     delay: 3,
   },
   {
-    src: " https://static.thenounproject.com/png/424728-200.png",
-    location: {x: 750, y: 600},
+    src: ' https://static.thenounproject.com/png/424728-200.png',
+    location: { x: 750, y: 600 },
     delay: 2,
   },
   {
-    src: "https://cdn-icons-png.flaticon.com/512/1310/1310190.png",
-    location: {x: 500, y: 300},
+    src: 'https://cdn-icons-png.flaticon.com/512/1310/1310190.png',
+    location: { x: 500, y: 300 },
     delay: 1,
   },
 ];
 
-function createImageElement(src, {x, y}, delay) {
-  const img = document.createElement("img");
-  img.classList.add("bg-png");
+function createImageElement(src, { x, y }, delay) {
+  const img = document.createElement('img');
+  img.classList.add('bg-png');
   img.src = src;
-  img.style.position = "absolute";
-  img.style.boxSizing = "border-box";
-  img.style.width = "50px";
+  img.style.position = 'absolute';
+  img.style.boxSizing = 'border-box';
+  img.style.width = '50px';
   img.style.left = `${x}px`;
   img.style.top = `${y}px`;
-  img.style.zIndex = "-1";
-  img.style.opacity = "0.1";
+  img.style.zIndex = '-1';
+  img.style.opacity = '0.1';
   img.style.animationDelay = `${delay}s`;
 
-  document.querySelector(".background-container").appendChild(img);
+  document.querySelector('.background-container').appendChild(img);
 }
 
 function initialize() {
-  pngImages.forEach(({src, location, delay}) => {
+  pngImages.forEach(({ src, location, delay }) => {
     createImageElement(src, location, delay);
   });
 }
--- quiz.js
+++ quiz.js
@@ -1,35 +1,36 @@
 //user inputs
-const userNameInput = document.querySelector("#inputUserName");
+const userNameInput = document.querySelector('#inputUserName');
 
 //display
-const userNameDisplay = document.querySelector("#userName"),
-  chosenSubjectDisplay = document.querySelector("#chosenSubject"),
-  chosenGradeDisplay = document.querySelector("#chosenGrade"),
-  scoreDisplay = document.querySelector("#score"),
-  timerDisplay = document.querySelector("#timer"),
-  explanationDisplay = document.querySelector("#explanationDisplay"),
-  scopeDisplay = document.querySelector("#scope");
+const userNameDisplay = document.querySelector('#userName'),
+  chosenSubjectDisplay = document.querySelector('#chosenSubject'),
+  chosenGradeDisplay = document.querySelector('#chosenGrade'),
+  scoreDisplay = document.querySelector('#score'),
+  timerDisplay = document.querySelector('#timer'),
+  explanationDisplay = document.querySelector('#explanationDisplay'),
+  scopeDisplay = document.querySelector('#scope');
 
 //container
-const quizBodyContainer = document.querySelector("#quizBodyContainer"),
-  quizScopeContainer = document.querySelector("#quizScopeContainer"),
+const quizBodyContainer = document.querySelector('#quizBodyContainer'),
+  quizScopeContainer = document.querySelector('#quizScopeContainer'),
   quizScopeHeaderContainer = document.querySelector(
-    "#quizScopeHeaderContainer",
+    '#quizScopeHeaderContainer',
   ),
   quizQuestionsOptionsContainer = document.querySelector(
-    "#quizQuestionsOptionsContainer",
+    '#quizQuestionsOptionsContainer',
   ),
-  explanationContainer = document.querySelector("#explanationContainer");
+  explanationContainer = document.querySelector('#explanationContainer'),
+  quizMechanicsContainer = document.getElementById('quizMechanicsContainer');
 
 //Buttons
-const answerButton = document.querySelectorAll("#answerButton"),
-  startGame = document.querySelector("#quizStartButton"),
-  nextQuestion = document.querySelector("#nextQuestion");
+const answerButton = document.querySelectorAll('#answerButton'),
+  startGame = document.querySelector('#quizStartButton'),
+  nextQuestion = document.querySelector('#nextQuestion');
 
 //subjects
 
-const subjects = document.getElementById("subjects"),
-  grades = document.getElementById("gradeLevel");
+const subjects = document.getElementById('subjects'),
+  grades = document.getElementById('gradeLevel');
 
 let chosenSubject,
   chosenGrade,
@@ -43,21 +44,21 @@ function getScope() {
     selectedSubject = subjects.value,
     selectedGrade = grades.value;
 
-  console.log("Selected Subject :" + selectedSubject);
+  console.log('Selected Subject :' + selectedSubject);
 
-  if (userName && selectedSubject !== "" && selectedGrade) {
+  if (userName && selectedSubject !== '' && selectedGrade) {
     userNameDisplay.innerHTML = userName;
     chosenSubject = selectedSubject;
     chosenSubjectDisplay.innerHTML = chosenSubject;
     chosenGrade = selectedGrade;
     chosenGradeDisplay.innerHTML = chosenGrade;
   } else {
-    alert("Please fill out all fields");
+    alert('Please fill out all fields');
     return;
   }
 
-  quizMechanicsContainer.style.display = "none";
-  quizScopeContainer.style.display = "block";
+  quizScopeContainer.style.display = 'block';
+  quizMechanicsContainer.style.display = 'none';
 
   const scopeDescription = scopes[chosenSubject][chosenGrade].scopeDescription;
   const scopeDetails = scopes[chosenSubject][chosenGrade].scopeDetails;
@@ -66,7 +67,7 @@ function getScope() {
   // Generate the bullet points as a string using map
   const bulletPointsHTML = scopeDetails
     .map((detail) => `<li>${detail}</li>`)
-    .join(""); // Join the array elements into a single string
+    .join(''); // Join the array elements into a single string
 
   const scopeHTML = `
   <p>${scopeDescription}</p>
@@ -79,14 +80,14 @@ function getScope() {
   scopeDisplay.innerHTML = scopeHTML;
 }
 
-startGame.addEventListener("click", () => {
+startGame.addEventListener('click', () => {
   score = 0; // Reset the score
   currentQuestionIndex = 0; // Reset the current question index
-  quizQuestionsOptionsContainer.innerHTML = ""; // Clear the quiz container
+  quizQuestionsOptionsContainer.innerHTML = ''; // Clear the quiz container
 
-  quizScopeContainer.style.display = "none";
-  quizScopeHeaderContainer.style.display = "block";
-  quizBodyContainer.style.display = "block";
+  quizScopeContainer.style.display = 'none';
+  quizScopeHeaderContainer.style.display = 'block';
+  quizBodyContainer.style.display = 'block';
 
   // Shuffle the questions only once when the quiz starts
   const shuffledQuestions = shuffleArray(
@@ -113,82 +114,81 @@ function shuffleArray(array) {
 }
 
 function handleOptionClick(currentQuestion, e) {
-  const answerButtons = document.querySelectorAll(".answer-button");
+  const answerButtons = document.querySelectorAll('.answer-button');
 
-  if (e.target.classList.contains("answer-button")) {
+  if (e.target.classList.contains('answer-button')) {
     answerButtons.forEach((button) => {
       // Disable all the answer buttons to prevent changing the answer
       button.disabled = true;
       if (button === e.target) {
-        const selectedOptionText = e.target.textContent.split(") ")[1];
+        const selectedOptionText = e.target.textContent.split(') ')[1];
         const selectedOption = currentQuestion.options.find(
           (option) => option.text === selectedOptionText,
         );
 
         if (
           selectedOption &&
-          selectedOption.hasOwnProperty("isCorrect") &&
+          selectedOption.hasOwnProperty('isCorrect') &&
           selectedOption.isCorrect
         ) {
           score++;
           // If the selected option is correct, turn the button green
-          e.target.classList.add("border-success", "border-3");
+          e.target.classList.add('border-success', 'border-3');
         } else {
-          // Show the correct answer when the user selects the wrong option
-          explanationContainer.style.display = "block";
-          explanationDisplay.classList.add("text-danger", "my-3");
+          explanationContainer.style.display = 'block';
+          explanationDisplay.classList.add('text-danger', 'my-3');
           explanationDisplay.innerHTML =
             getCorrectAnswerExplanation(currentQuestion);
           // If the selected option is wrong, turn the button red
-          e.target.classList.add("border-danger", "border-3");
+          e.target.classList.add('border-danger', 'border-3');
         }
       } else {
-        const buttonOptionText = button.textContent.split(") ")[1];
+        const buttonOptionText = button.textContent.split(') ')[1];
         const buttonOption = currentQuestion.options.find(
           (option) => option.text === buttonOptionText,
         );
 
         if (
           buttonOption &&
-          buttonOption.hasOwnProperty("isCorrect") &&
+          buttonOption.hasOwnProperty('isCorrect') &&
           buttonOption.isCorrect
         ) {
           // For other buttons, if the option is correct, turn it green
-          button.classList.add("border-success", "border-3");
+          button.classList.add('border-success', 'border-3');
         } else {
-          button.classList.add("disabled-answer-button");
+          button.classList.add('disabled');
         }
       }
     });
 
     // Create Next Question Button
-    const nextQuestionButton = document.createElement("button");
-    nextQuestionButton.setAttribute("id", "nextQuestion");
+    const nextQuestionButton = document.createElement('button');
+    nextQuestionButton.setAttribute('id', 'nextQuestion');
     nextQuestionButton.classList.add(
-      "btn",
-      "btn-outline-secondary",
-      "btn-lg",
-      "my-3",
+      'btn',
+      'btn-outline-secondary',
+      'btn-lg',
+      'my-3',
     );
-    nextQuestionButton.textContent = "Next Question";
+    nextQuestionButton.textContent = 'Next Question';
 
     const nextQuestionContainer = document.getElementById(
-      "nextQuestionContainer",
+      'nextQuestionContainer',
     );
     // Use appendChild to add the nextQuestionButton to the nextQuestionContainer
     nextQuestionContainer.appendChild(nextQuestionButton);
 
-    nextQuestionButton.addEventListener("click", () => {
+    nextQuestionButton.addEventListener('click', () => {
       // Clear visual indication and re-enable the answer buttons
       answerButtons.forEach((button) => {
-        button.classList.remove("is-valid", "is-invalid");
+        button.classList.remove('is-valid', 'is-invalid');
         button.disabled = false;
       });
 
-      quizQuestionsOptionsContainer.innerHTML = "";
+      quizQuestionsOptionsContainer.innerHTML = '';
       currentQuestionIndex++;
-      explanationDisplay.innerHTML = "";
-      explanationContainer.style.display = "none";
+      explanationDisplay.innerHTML = '';
+      explanationContainer.style.display = 'none';
       createQuestions();
 
       // Remove the "Next Question" button
@@ -223,37 +223,37 @@ function createQuestions() {
   ) {
     const currentQuestion = shuffledQuestions[currentQuestionIndex];
     // Create a div to hold the question and options
-    const questionDiv = document.createElement("div");
-    questionDiv.classList.add("quiz-question-container");
+    const questionDiv = document.createElement('div');
+    questionDiv.classList.add('quiz-question-container');
 
     // Display the question
-    const questionElement = document.createElement("h3");
-    questionElement.classList.add("my-3");
+    const questionElement = document.createElement('h3');
+    questionElement.classList.add('my-3');
     questionElement.textContent =
-      currentQuestionIndex + 1 + ".) " + currentQuestion.question;
+      currentQuestionIndex + 1 + '.) ' + currentQuestion.question;
     questionDiv.appendChild(questionElement);
 
     quizQuestionsOptionsContainer.appendChild(questionDiv);
 
-    const optionsDiv = document.createElement("div");
-    optionsDiv.classList.add("quiz-options-container");
+    const optionsDiv = document.createElement('div');
+    optionsDiv.classList.add('quiz-options-container');
 
-    const letters = ["a", "b", "c", "d"];
+    const letters = ['a', 'b', 'c', 'd'];
     for (let i = 0; i < currentQuestion.options.length; i++) {
       const option = currentQuestion.options[i];
-      const optionItemElement = document.createElement("button");
-      optionItemElement.setAttribute("id", "answerButton");
+      const optionItemElement = document.createElement('button');
+      optionItemElement.setAttribute('id', 'answerButton');
       optionItemElement.classList.add(
-        "btn",
-        "btn-outline-primary",
-        "btn-lg",
-        "m-1",
-        "w-50",
-        "answer-button",
+        'btn',
+        'btn-outline-primary',
+        'btn-lg',
+        'm-1',
+        'w-50',
+        'answer-button',
       );
-      optionItemElement.textContent = letters[i] + ") " + option.text;
+      optionItemElement.textContent = letters[i] + ') ' + option.text;
 
-      optionItemElement.addEventListener("click", (e) => {
+      optionItemElement.addEventListener('click', (e) => {
         handleOptionClick(currentQuestion, e);
       });
       optionsDiv.appendChild(optionItemElement);
@@ -261,7 +261,7 @@ function createQuestions() {
 
     quizQuestionsOptionsContainer.appendChild(optionsDiv);
 
-    console.log("Create Question", currentQuestionIndex);
+    console.log('Create Question', currentQuestionIndex);
   }
 }
 
@@ -271,18 +271,18 @@ function countDownTimer() {
 
   if (currentQuestionIndex === shuffledQuestions.length) {
     clearInterval(countDownTimer);
-    quizQuestionsOptionsContainer.innerHTML = "";
-    quizScopeHeaderContainer.style.display = "none";
-    quizBodyContainer.style.display = "none";
-    explanationContainer.innerHTML = "";
+    quizQuestionsOptionsContainer.innerHTML = '';
+    quizScopeHeaderContainer.style.display = 'none';
+    quizBodyContainer.style.display = 'none';
+    explanationContainer.innerHTML = '';
     getResult();
   } else if (timer === 0) {
     clearInterval(countDownTimer);
-    quizQuestionsOptionsContainer.innerHTML = "";
-    quizScopeHeaderContainer.style.display = "none";
-    explanationContainer.style.display = "none";
-    quizBodyContainer.style.display = "none";
-    explanationContainer.innerHTML = "";
+    quizQuestionsOptionsContainer.innerHTML = '';
+    quizScopeHeaderContainer.style.display = 'none';
+    explanationContainer.style.display = 'none';
+    quizBodyContainer.style.display = 'none';
+    explanationContainer.innerHTML = '';
     getResult();
   }
 }
@@ -292,20 +292,20 @@ function formatTime(seconds) {
   const secondsRemaining = seconds % 60;
   const formattedTime = `${minutes
     .toString()
-    .padStart(2, "0")}:${secondsRemaining.toString().padStart(2, "0")}`;
+    .padStart(2, '0')}:${secondsRemaining.toString().padStart(2, '0')}`;
   return formattedTime;
 }
 
 function getResult() {
-  const resultDisplay = document.getElementById("score");
-  const resultContainer = document.getElementById("resultContainer");
-  const feedbackDisplay = document.getElementById("feedback");
+  const resultDisplay = document.getElementById('score');
+  const resultContainer = document.getElementById('resultContainer');
+  const feedbackDisplay = document.getElementById('feedback');
 
-  resultContainer.style.display = "block";
+  resultContainer.style.display = 'block';
 
   resultDisplay.textContent = score;
 
   score >= 10
-    ? (feedbackDisplay.textContent = "You Passed !")
-    : (feedbackDisplay.textContent = "You failed, sorry!");
+    ? (feedbackDisplay.textContent = 'You Passed !')
+    : (feedbackDisplay.textContent = 'You failed, sorry!');
 }
